aa2ff192d57d8a220073afcfc9d9e8b0e0ef9c05
HereComesTheKing <9312754+herecomestheking@user.noreply.gitee.com>
2024-03-10 14:47:04 +0800

    修改了登陆界面layout

--- OfficeAutomationSocieties.miniprogram/pages/signin/signin.js
+++ OfficeAutomationSocieties.miniprogram/pages/signin/signin.js
@@ -1,33 +1,62 @@
 // pages/signin/signin.js
+// const userhelper = require("/utils/helper/userhelper.js");
+
 Page({
   data: {
-    username:"",
-    password:"",
-    isLoading:false
+    loginmodel: {
+      name: "",
+      password: ""
+    },
+    isLoading: false
   },
-  
+
   // 点击登录按钮，执行登录操作
-  login: function(event) {    
+  login: function (data) {
+    // 判断用户名与密码是否合法
+    var username = data.detail.value.username;
+    var password = data.detail.value.password;
+    if(username == '' || password == '') // 暂时这样，需要指定账号密码规则。
+    {
+      console.log("用户名或密码非法");
+      return;
+    }
+
+    // 赋值usermodel
+    this.setData({
+      loginmodel: {
+        name: username,
+        password: password
+      }
+    });
+    console.log("LoginModel:",this.data.loginmodel);
+
     // 加载条
     this.setData({
       isLoading: true
     });
-    
+
+    // 异步获取登录信息
+    // userhelper.login(
+
+    // )
+
     // 模拟异步操作
-    setTimeout(() => {
-      this.setData({
-        isLoading: false
-      });
-      // wx.redirectTo({
-      //   url: '/pages/index/index',
-      // })
-      wx.switchTab({
-        url: '../index/index',
-      }).then(() => console.log("Login"));
-    }, 500);
+    // setTimeout(() => {
+    //   this.setData({
+    //     isLoading: false
+    //   });
+    //   wx.switchTab({
+    //     url: '../index/index',
+    //   }).then(() => console.log("Login"));
+    // }, 500);
+  },
+
+  checkLoginStatus: function () {
+
   },
 
   onload() {
     wx.hideTabBar();
+    this.checkLoginStatus();
   }
 })
