1cb0585543649db10c1d4667c46b60e48066c81d
HereComesTheKing <9312754+herecomestheking@user.noreply.gitee.com>
2024-04-09 22:57:28 +0800

    主页列表样式修改

--- OfficeAutomationSocieties.miniprogram/pages/index/index.js
+++ OfficeAutomationSocieties.miniprogram/pages/index/index.js
@@ -1,50 +1,31 @@
 // index.js
 const app = getApp()
 Page({
-  data:{
-    isLoading:true,
-    task : [{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"},{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"},{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"},{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"},{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"},{endTime: "2024-04-12",
-    isDone: false,
-    startTime: "2024-04-09",
-    toDo: "完成小程序"}],
-    taskCount : 50,
-    doingTask : [],
-    timeOutTask : [],
-    willDoTask : []
+  data: {
+    task: [],
+    taskCount: 0,
+    doingTask: [],
+    timeOutTask: [],
+    willDoTask: []
   },
 
-  onShow() {
+  onLoad() {
     wx.showTabBar(true);
-    
+
     app.globalData.user.taskNotes.forEach(item => {
-      if(item.isDone !== true){
+      if (item.isDone !== true) {
         this.setData({
-          taskCount : this.data.taskCount + 1
+          taskCount: this.data.taskCount + 1
         })
         const starkTime = new Date(item.startTime)
         const endTime = new Date(item.endTime)
         const now = new Date()
-        if(endTime < now){
+        if (endTime < now) {
           this.data.timeOutTask.push(item)
           return
         }
 
-        if(starkTime > now){
+        if (starkTime > now) {
           this.data.willDoTask.push(item)
           return
         }
@@ -53,16 +34,31 @@ Page({
       }
     });
 
+    // this.data.task.push();
+
     this.setData({
-      isLoading:true,
-      task : app.globalData.user.taskNotes
+      task: app.globalData.user.taskNotes,
+      willDoTask: this.data.willDoTask,
+      doingTask: this.data.doingTask,
+      timeOutTask: this.data.timeOutTask
     })
 
-    console.log(this.data.task)
+    console.log("所有任务:", this.data.task)
+    console.log("未来任务", this.data.willDoTask)
+    console.log("正在中任务:", this.data.doingTask)
+    console.log("超时任务:", this.data.timeOutTask)
+
     // setInterval(()=>{
     //   this.setData({
     //     isLoading:false
     //   })
     // },300);
+  },
+  formatDate(dateStr) {
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}年${month}月${day}日`;
   }
 })
