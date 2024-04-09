dc5c72477ac96f5dac3464b4792675f9f606abf4
李嘉俊 <2383690354@qq.com>
2024-04-09 22:17:46 +0800

    项目详情-接入基本数据

--- OfficeAutomationSocieties.miniprogram/pages/project/project_detail/project_detail.js
+++ OfficeAutomationSocieties.miniprogram/pages/project/project_detail/project_detail.js
@@ -1,16 +1,33 @@
+import { apiurl } from "../../../utils/api.js";
 const app = getApp()
 
 Page({
   data: {
-    thisProject: {}
+    thisProject: {},
+    members : []
   },
   onLoad(options) {
     const a = app.globalData.projects.find(item => item.id === options.id)
     if(a == null){
       console.log("有问题",app.globalData.projects,options.id)
-      return}
+      return
+    }
     this.setData({
       thisProject : a
+    })
+
+    wx.request({
+      method: "GET",
+      header: {
+        authorization: "Bearer " + app.globalData.jwt
+      },
+      url: apiurl + 'Project/GetProjectMember/'+options.id,
+      success: (member) => {
+        if(member.statusCode !== 200) return
+        this.setData({
+          members : member.data
+        })
+      }
     })
   }
 })
